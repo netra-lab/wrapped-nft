6aef4d8b7b1f2be4385862a84862db5ed72bc4a0
farreldarian <farreldarian@users.noreply.github.com>
2022-10-09 15:09:44 +0700

    Fix error on compile

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,3 +1,9 @@
-import './batch-wrap'
-import './batch-unwrap'
-import './whitelist-collection'
+import { existsSync } from 'fs'
+import { join } from 'path'
+
+const compiled = existsSync(join(__dirname, '../types'))
+if (compiled) {
+  require('./batch-wrap')
+  require('./batch-unwrap')
+  require('./whitelist-collection')
+}
